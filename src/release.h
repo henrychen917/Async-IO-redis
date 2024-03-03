2b12646780646b810891c696e90b1eec4cf73ea2
henrychen917 <henrychen917@gmail.com>
2024-03-03 15:55:22 +0900

    Add files via upload

---
+++ src/release.h
@@ -0,0 +1,5 @@
+#define REDIS_GIT_SHA1 "9d0158bf"
+#define REDIS_GIT_DIRTY "0"
+#define REDIS_BUILD_ID "host216.pa.igp.shibaura-it.ac.jp-1704272794"
+#include "version.h"
+#define REDIS_BUILD_ID_RAW REDIS_VERSION REDIS_BUILD_ID REDIS_GIT_DIRTY REDIS_GIT_SHA1
